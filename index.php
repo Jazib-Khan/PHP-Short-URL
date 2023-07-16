d622461e8630552a60d80529585f416cb52d642c
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-07-16 14:53:41 +0100

    frontend adjustments

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
 </head>
 <body>
     <h1>URL Shortener</h1>
+    <h2>Input a URL to shorten it</h2>
     <form action="shortUrl.php" method="post">
-        <input type="text" name="inputUrl" placeholder="Enter URL">
+        <input type="text" name="inputUrl" placeholder="Enter URL" autocomplete="off" size="80">
         <input type="submit" value="Shorten">
     </form>
 </body>
