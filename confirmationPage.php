f306c58bebc741dc9945c059c42daad24b960213
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-07-16 14:35:02 +0100

    short URL fetched on confirmation page

--- confirmationPage.php
+++ confirmationPage.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
-</body>
-</html>
+<?php
+    $shortUrl = $_GET["shortUrl"];
+
+    $db = new PDO("mysql:host=localhost;dbname=urls", "root", "");
+    $sql = "SELECT inputUrl FROM urlshortener WHERE shortUrl='$shortUrl'";
+    $result = $db->query($sql);
+    $row = $result->fetch();
+
+    if ($row) {
+        echo "Your short URL is: <a href='$row[0]'>localhost/" . "$shortUrl</a>";
+    }
+?>
